8fd167f51c82db0cf96d36bee35f8db06453cbc2
Aaron Anderson <aaronanderson@acm.org>
2016-08-22 23:34:00 -0500

    updated to wildfly-swarm 2016.9-SNAPSHOT

---
+++ swarm-oidc/src/main/java/com/mercer/cpsg/swarm/oidc/internal/SecuredImpl.java
@@ -0,0 +1,103 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mercer.cpsg.swarm.oidc.internal;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.jboss.shrinkwrap.api.Node;
+import org.jboss.shrinkwrap.api.asset.ByteArrayAsset;
+import org.jboss.shrinkwrap.api.asset.NamedAsset;
+import org.jboss.shrinkwrap.impl.base.ArchiveBase;
+import org.jboss.shrinkwrap.impl.base.AssignableBase;
+import org.jboss.shrinkwrap.impl.base.importer.zip.ZipImporterImpl;
+import org.wildfly.swarm.bootstrap.util.BootstrapProperties;
+import org.wildfly.swarm.spi.api.JARArchive;
+import org.wildfly.swarm.undertow.descriptors.SecurityConstraint;
+import org.wildfly.swarm.undertow.descriptors.WebXmlAsset;
+
+import com.mercer.cpsg.swarm.oidc.Secured;
+
+public class SecuredImpl extends AssignableBase<ArchiveBase<?>> implements Secured {
+
+	/**
+	 * Constructs a new instance using the underlying specified archive, which
+	 * is required
+	 *
+	 * @param archive
+	 */
+	public SecuredImpl(ArchiveBase<?> archive) {
+		super(archive);
+
+		Node node = getArchive().as(JARArchive.class).get("WEB-INF/web.xml");
+		if (node == null) {
+			this.asset = new WebXmlAsset();
+			getArchive().as(JARArchive.class).add(this.asset);
+		} else {
+			NamedAsset asset = (NamedAsset) node.getAsset();
+			if (!(asset instanceof WebXmlAsset)) {
+				this.asset = new WebXmlAsset(asset.openStream());
+				getArchive().as(JARArchive.class).add(this.asset);
+			} else {
+				this.asset = (WebXmlAsset) asset;
+			}
+		}
+
+		InputStream oidcJson = Thread.currentThread().getContextClassLoader().getResourceAsStream("oidc.json");
+		if (oidcJson == null) {
+
+			String appArtifact = System.getProperty(BootstrapProperties.APP_ARTIFACT);
+
+			if (appArtifact != null) {
+				try (InputStream in = ClassLoader.getSystemClassLoader().getResourceAsStream("_bootstrap/" + appArtifact)) {
+					ZipImporterImpl importer = new ZipImporterImpl(archive);
+					importer.importFrom(in);
+					Node jsonNode = archive.get("oidc.json");
+					if (jsonNode == null) {
+						jsonNode = archive.get("WEB-INF/oidc.json");
+					}
+
+					if (jsonNode != null && jsonNode.getAsset() != null) {
+						oidcJson = jsonNode.getAsset().openStream();
+					}
+				} catch (IOException e) {
+					// ignore
+					// e.printStackTrace();
+				}
+			}
+		}
+
+		// Setup web.xml
+		this.asset.setContextParam("resteasy.scan", "true");
+
+		if (oidcJson != null) {
+			getArchive().as(JARArchive.class).add(new ByteArrayAsset(oidcJson), "WEB-INF/oidc.json");
+		} else {
+			// not adding it.
+		}
+	}
+
+	@Override
+	public SecurityConstraint protect() {
+		return this.asset.protect();
+	}
+
+	@Override
+	public SecurityConstraint protect(String urlPattern) {
+		return this.asset.protect(urlPattern);
+	}
+
+	private WebXmlAsset asset;
+}
--- test/src/main/webapp/index.jsp
+++ test/src/main/webapp/index.jsp
@@ -1,2 +1,2 @@
-<%@ page import="com.mercer.cpsg.swarm.oidc.OIDCPrincipal" %>
+<%@ page import="com.mercer.cpsg.swarm.oidc.deployment.OIDCPrincipal" %>
 Hello <%= ((OIDCPrincipal)request.getUserPrincipal()).getPreferredUserName() %>!!!
